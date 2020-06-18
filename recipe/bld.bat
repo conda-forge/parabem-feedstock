a2a0862cc0d2b49795cd36339dcb601445631d89
looooo <sppedflyer@gmail.com>
2020-06-18 11:43:28 +0200

    disable mpi on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,6 +4,7 @@ cd build
 cmake -G "Ninja" ^
       -D CMAKE_INSTALL_PREFIX:FILEPATH=%LIBRARY_PREFIX% ^
       -D CMAKE_BUILD_TYPE=Release ^
+      -D BUILD_WITH_MPI:BOOL=OFF ^
       ..
 
 if errorlevel 1 exit 1
