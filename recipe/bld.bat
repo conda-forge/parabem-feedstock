8e8cba5db70677f1dacbbf211e9fa0482136bb56
looooo <sppedflyer@gmail.com>
2024-07-27 17:51:27 +0200

    update to 1.0.0

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,7 @@
-mkdir build
-cd build
-
-cmake -G "Ninja" ^
+cmake -G "Ninja" -B build -S . ^
       -D CMAKE_INSTALL_PREFIX:FILEPATH=%LIBRARY_PREFIX% ^
       -D CMAKE_BUILD_TYPE=Release ^
-      -D BUILD_WITH_MPI:BOOL=OFF ^
-      ..
+      -D BUILD_WITH_MPI:BOOL=OFF
 
 if errorlevel 1 exit 1
 ninja install
-if errorlevel 1 exit 1
