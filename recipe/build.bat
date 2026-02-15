362b3e8620b9f44006a53a8e4821b9f476d26349
Lorenz Lechner <email.lorenz.lechner@gmail.com>
2026-02-15 18:27:09 +0100

    try to fix windows

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,6 +1,8 @@
 cmake -G "Ninja" -B build -S . ^
-      -D CMAKE_INSTALL_PREFIX:FILEPATH=%LIBRARY_PREFIX% ^
+      -D CMAKE_INSTALL_PREFIX:FILEPATH="%PREFIX%\Library" ^
       -D CMAKE_BUILD_TYPE=Release ^
+      -D Python_EXECUTABLE:FILEPATH="%PYTHON%" ^
+      -D PYTHON_EXECUTABLE:FILEPATH="%PYTHON%" ^
       -D BUILD_WITH_MPI:BOOL=OFF
 
 if errorlevel 1 exit 1
